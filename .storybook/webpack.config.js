16c83e3c0535fddfbe33b17cae058715d62b98eb
ChrisTerBeke <c.terbeke@ultimaker.com>
2018-06-15 13:41:06 +0200

    Button with knobs

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,30 +1,38 @@
 const path = require("path");
+const webpack = require('webpack');
 
 module.exports = {
+    devtool: "#inline-source-map",
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.json']
     },
     module: {
         rules: [
             {
-                test: /\.tsx/,
+                test: /\.(ts|tsx)$/,
                 loader: 'awesome-typescript-loader',
-                include: path.resolve(__dirname, '../src')
+                include: path.resolve(__dirname, '../src'),
+                exclude: /node_modules/
             },
             {
                 test: /\.sass$/,
                 loaders: ['style-loader', 'css-loader', 'sass-loader'],
-                include: path.resolve(__dirname, '../src')
+                include: path.resolve(__dirname, '../src'),
+                exclude: /node_modules/
             },
             {
                 test: /\.(woff|woff2|eot|ttf|png|svg|jpg|ico)$/,
                 loader: 'file-loader',
                 include: path.resolve(__dirname, '../src'),
+                exclude: /node_modules/,
                 options: {
                     name: 'static/[path][name].[ext]?[hash]',
                     context: 'src'
                 }
             }
         ]
-    }
-};
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
--- src/stories/button.stories.tsx
+++ src/stories/button.stories.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs/react';
 
 import Button from '../components/button'
 
-storiesOf('Button', module)
-  .add('Simple', () => <Button onClickHandler={action('clicked')}>Hello Button</Button>)
-  .add('Loading', () => <Button onClickHandler={action('clicked')} style="primary" showSpinner>Hello Button</Button>)
-  .add('Primary', () => <Button onClickHandler={action('clicked')} style="primary">Hello Button</Button>);
+const stories = storiesOf('Button', module);
+stories.addDecorator(withKnobs);
+
+stories.add('Primary', () => 
+    <Button onClickHandler={action('clicked')} showSpinner={boolean('Loading', false)} style="primary">
+        {text('Label', 'Hello Storybook')}
+    </Button>
+);
