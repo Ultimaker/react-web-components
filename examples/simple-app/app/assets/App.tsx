fb3dd12d21bc56e892408ca0f2f92e87fb78c22a
Alan <a.woosnam@ultimaker.com>
2018-06-15 14:02:14 +0200

    undo testing

--- examples/simple-app/app/assets/App.tsx
+++ examples/simple-app/app/assets/App.tsx
@@ -1,12 +1,8 @@
 // Copyright (c) 2018 Ultimaker B.V.
 import * as React from 'react';
 
-import InputField from '../../../../src/components/input_field'
 export class App extends React.Component {
     render(): JSX.Element {
-        return <div>
-            <InputField type="text" onChangeHandler={() => {}}/>
-            <InputField type="textarea" focusOnLoad onChangeHandler={() => {}}/>
-        </div>
+        return <div>Hello world!</div>
     }
 }
