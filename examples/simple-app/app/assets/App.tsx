cf2ef462c3744321fab75d2218e115e3ebe34a73
Alan <a.woosnam@ultimaker.com>
2018-06-15 13:55:29 +0200

    expand InputField with type textarea

--- examples/simple-app/app/assets/App.tsx
+++ examples/simple-app/app/assets/App.tsx
@@ -1,8 +1,12 @@
 // Copyright (c) 2018 Ultimaker B.V.
 import * as React from 'react';
 
+import InputField from '../../../../src/components/input_field'
 export class App extends React.Component {
     render(): JSX.Element {
-        return <div>Hello world!</div>
+        return <div>
+            <InputField type="text" onChangeHandler={() => {}}/>
+            <InputField type="textarea" focusOnLoad onChangeHandler={() => {}}/>
+        </div>
     }
 }
