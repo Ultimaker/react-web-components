c4f61d8e47a37845952d65220fdf0db70107ada2
ChrisTerBeke <c.terbeke@ultimaker.com>
2018-06-15 11:56:43 +0200

    Add loading button

--- src/stories/button.stories.tsx
+++ src/stories/button.stories.tsx
@@ -6,4 +6,5 @@ import Button from '../components/button'
 
 storiesOf('Button', module)
   .add('Simple', () => <Button onClickHandler={action('clicked')}>Hello Button</Button>)
+  .add('Loading', () => <Button onClickHandler={action('clicked')} additionalClasses="btn--primary" showSpinner>Hello Button</Button>)
   .add('Primary', () => <Button onClickHandler={action('clicked')} additionalClasses="btn--primary">Hello Button</Button>);
