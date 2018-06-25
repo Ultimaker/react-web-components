3373e3a54404a4e9e1cd8a2e5747f94aa15c5e7d
Alan <a.woosnam@ultimaker.com>
2018-06-25 10:21:43 +0200

    make footer more generic

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react';
-import { i18nc } from '../utils/i18n';
 
-export interface FooterProps {
-  aboutBtnHandler: () => void;
-}
-
-const Footer: React.StatelessComponent<FooterProps> = ({ aboutBtnHandler }) => {
+const Footer: React.StatelessComponent = ({ children }) => {
 
   return (
     <footer className="app__footer">
-      <a onClick={aboutBtnHandler}>{i18nc("Footer About link", "About")}</a>
+      {children}
     </footer>
   );
 };
