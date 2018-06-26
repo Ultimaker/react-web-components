ffe47d48cec7ceabb5e61f009b158a174548980a
Alan <a.woosnam@ultimaker.com>
2018-06-26 14:15:01 +0200

    make Divider margin optional

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -1,21 +1,25 @@
 import * as React from 'react';
+import * as classNames from 'classnames';
 
 export type DividerDirection = 'horizontal' | 'vertical';
 
 export interface DividerProps {
   /** Direction to position the divider: 'horizontal' | 'vertical' */
   direction?: DividerDirection;
+  /** If true a margin will be applied to the divider */
+  margin?: boolean;
 }
 
-export const Divider: React.StatelessComponent<DividerProps> = ({ direction }) => {
+export const Divider: React.StatelessComponent<DividerProps> = ({ direction, margin }) => {
 
   return (
-    <div className={`divider divider--${direction}`}></div>
+    <div className={classNames(`divider divider--${direction}`, { 'divider--with-margin': margin })}></div>
   );
 };
 
 Divider.defaultProps = {
-  direction: 'horizontal'
+  direction: 'horizontal',
+  margin: false
 };
 
 export default Divider;
