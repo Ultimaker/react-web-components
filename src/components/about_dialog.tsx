0f0fb0f85a1d20487c2210ccde3fb9c2c0d93251
Alan <a.woosnam@ultimaker.com>
2018-06-26 17:29:09 +0200

    add AboutDialog component and story

---
+++ src/components/about_dialog.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { i18nc, i18nc_format } from '../utils/i18n';
+
+import Button from '../components/button';
+import CloseButton from "../components/close_button";
+import Modal from "../components/modal";
+import Tile from "../components/tile";
+
+export interface AboutDialogProps {
+  /** List of open source components used */
+  componentsList: OpenSourceComponent[];
+  /** Name of the application */
+  appName: string;
+  /** Version number of the application */
+  versionNumber: string;
+  /** Dialog will be displayed when true */
+  isOpen: boolean;
+  /** Function to close the dialog */
+  closeHandler: () => void;
+}
+
+interface OpenSourceComponent {
+  name: string;
+  license: string;
+  url: string;
+}
+
+
+function licenseList(componentsList: OpenSourceComponent[]): JSX.Element[] {
+  return componentsList.map(component => {
+    return (<tr key={component.name}>
+      <td className="about-component-name"><a href={component.url} target="_blank">{component.name}</a></td>
+      <td className="about-component-license"><a href={"https://spdx.org/licenses/" + component.license + ".html"} target="_blank">{i18nc_format("About dialog", "license %{license}", { license: component.license })}</a></td>
+    </tr>);
+  });
+}
+
+const AboutDialog: React.StatelessComponent<AboutDialogProps> =
+  ({ componentsList, isOpen, versionNumber, closeHandler, appName }) => {
+
+    return (
+      <div className="about-dialog">
+        <Modal isOpen={isOpen} onOverlayClickHandler={closeHandler}>
+          <Tile>
+            <div className="about-dialog__header">{i18nc_format("About dialog", "About %{appName}", { appName })}</div>
+            <p>{i18nc_format("About dialog", "Version: %{versionNumber}", { versionNumber })}</p>
+            <p>{i18nc_format("About dialog", "%{appName} uses the following Open Source components:", { appName })}</p>
+            <table className="about-components-list">
+              <tbody>
+                {licenseList(componentsList)}
+              </tbody>
+            </table>
+            <div className="about-dialog__btn">
+              <Button onClickHandler={closeHandler}>Close</Button>
+            </div>
+          </Tile>
+        </Modal>
+      </div>
+    );
+  };
+
+export default AboutDialog;
--- src/stories/modal.stories.tsx
+++ src/stories/modal.stories.tsx
@@ -7,6 +7,7 @@ import { withInfo } from '@storybook/addon-info';
 
 import Modal from '../components/modal';
 import Popup from '../components/popup';
+import AboutDialog from '../components/about_dialog';
 
 const stories = storiesOf('Modal', module);
 
@@ -60,4 +61,29 @@ stories.add('Prompt popup', withInfo(
     inputDefaultValue={10}
     inputMin={1}
     inputMax={100} />
+));
+
+stories.add('About dialog', withInfo(
+  'Popup modal for user input'
+)(() =>
+  <AboutDialog
+    appName="Test app"
+    versionNumber="1.0.0"
+    isOpen={boolean('isOpen', true)}
+    closeHandler={action('clicked')}
+    componentsList={[
+      { name: "array.prototype.includes", license: "MIT", url: "https://github.com/Steditor/es7-array.prototype.includes" },
+      { name: "classnames", license: "MIT", url: "https://github.com/JedWatson/classnames" },
+      { name: "gettext-extractor", license: "MIT", url: "https://github.com/lukasgeiter/gettext-extractor" },
+      { name: "gettext-parser", license: "MIT", url: "https://github.com/smhg/gettext-parser" },
+      { name: "lodash", license: "MIT", url: "https://lodash.com/" },
+      { name: "moment", license: "MIT", url: "http://momentjs.com/" },
+      { name: "node-gettext", license: "MIT", url: "https://github.com/alexanderwallin/node-gettext" },
+      { name: "react", license: "MIT", url: "https://reactjs.org/" },
+      { name: "react-collapse", license: "MIT", url: "https://github.com/nkbt/react-collapse" },
+      { name: "react-dom", license: "MIT", url: "https://reactjs.org/" },
+      { name: "react-motion", license: "MIT", url: "https://github.com/chenglou/react-motion" },
+      { name: "react-refetch", license: "MIT", url: "https://github.com/heroku/react-refetch" },
+      { name: "react-router-dom", license: "MIT", url: "https://github.com/ReactTraining/react-router" }
+    ]} />
 ));
