320365dda5d537cdf3af524189b7d93a18663503
Daniel Schiavini <d.schiavini@ultimaker.com>
2018-07-16 14:26:52 +0200

    Fixing build error

--- src/views/page_not_found.tsx
+++ src/views/page_not_found.tsx
@@ -8,16 +8,23 @@ import Tile from '../components/tile'
 // utils
 import { I18n } from '../utils/i18n'
 
+export interface PageNotFoundViewProps {
+    location: Location
+}
+
+export interface PageNotFoundViewState {
+}
+
 /**
  * The page not found view.
  */
-export class PageNotFoundView extends React.Component<LoginViewProps, LoginViewState> {
+export default class PageNotFoundView extends React.Component<PageNotFoundViewProps, PageNotFoundViewState> {
     render(): JSX.Element {
         return <Page title={I18n.translate("title page_not_found", "Page Not Found")} maxWidth={550}>
             <Tile padding="lg" align="center">
                 <p>
                     {I18n.translate("page_not_found", "The page could not be found.")}
-                    {this.props.location}
+                    {this.props.location.pathname}
                 </p>
             </Tile>
         </Page>
