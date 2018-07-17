84564556b255d1de9296e519d9b1f2c0a995dca6
Alan <a.woosnam@ultimaker.com>
2018-07-17 10:34:34 +0200

    add SlideInPanel component and story

---
+++ src/components/slide_in_panel.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { Motion, spring } from 'react-motion';
+import * as classNames from 'classnames';
+
+import CloseButton from "./close_button";
+
+export interface SlideInPanelProps {
+  /** Text to be displayed in the panel header */
+  headerTitle: string;
+  /** The panel will be open when true */
+  isOpen: boolean;
+  /** Called when the background overlay is clicked */
+  onOverlayClickHandler?: Function;
+  /** Width of panel. Include unit */
+  panelWidth?: string;
+}
+
+export class SlideInPanel extends React.Component<SlideInPanelProps, {}> {
+
+  _handleOverlayClick(e: React.MouseEvent<HTMLDivElement>): void {
+    const { onOverlayClickHandler } = this.props;
+
+    e.stopPropagation();
+
+    if (onOverlayClickHandler) {
+      onOverlayClickHandler();
+    }
+  }
+
+  render(): JSX.Element {
+    const { headerTitle, isOpen, panelWidth, children } = this.props;
+
+    const motion = { stiffness: 450, damping: 50 };
+    const classes = classNames('slide-in-panel', { isOpen });
+
+    return <div className={classes} onClick={(e) => this._handleOverlayClick(e)}>
+
+      <Motion style={{ x: spring(isOpen ? 0 : 100, motion) }}>
+        {({ x }) =>
+          <div className="slide-in-panel__container" style={{ transform: `translate3d(${x}%,0,0)`, width: panelWidth }}>
+            <div className="slide-in-panel__header">
+              {headerTitle}
+            </div>
+            <div className="slide-in-panel__content">
+              {children}
+            </div>
+          </div>
+        }
+      </Motion>
+
+    </div>
+
+  }
+}
+
+export default SlideInPanel;
