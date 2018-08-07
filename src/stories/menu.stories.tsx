f1c70b9e35468a3976d71342ef667fe7c96fbe2e
Alan <a.woosnam@ultimaker.com>
2018-08-07 11:52:23 +0200

    add the ability to show the context menu above trigger

--- src/stories/menu.stories.tsx
+++ src/stories/menu.stories.tsx
@@ -3,25 +3,24 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text, boolean, selectV2 } from '@storybook/addon-knobs/react';
 import styles from "@sambego/storybook-styles";
-import { State, Store } from '@sambego/storybook-state';
 import { withInfo } from '@storybook/addon-info';
 
 import ContextMenu from '../components/context_menu';
 import ContextMenuItem from '../components/context_menu_item';
-import DropDownMenu from '../components/drop_down_menu';
-import DropDownMenuItem from '../components/drop_down_menu_item';
 
 const stories = storiesOf('Menus', module);
 
-const store = new Store({
-  label: 'Item 1',
-});
-
-const options = {
+const offsetOptions = {
   Left: 'left',
   Right: 'right',
 };
-const defaultValue = 'left';
+const offsetDefaultValue = 'left';
+
+const directionOptions = {
+  Left: 'north',
+  Right: 'south',
+};
+const directionDefaultValue = 'south';
 
 stories.addDecorator(withKnobs)
   .addDecorator(styles({
@@ -34,7 +33,10 @@ stories.addDecorator(withKnobs)
 stories.add('Context menu', withInfo(
   'Context menu for additional actions'
 )(() =>
-  <ContextMenu menuWidth={200} menuOffsetDirection={selectV2('Menu Offset Direction', options, defaultValue)}>
+  <ContextMenu
+    menuWidth={200}
+    menuDirection={selectV2('Menu Direction', directionOptions, directionDefaultValue)}
+    menuOffsetDirection={selectV2('Menu Offset Direction', offsetOptions, offsetDefaultValue)}>
     <ContextMenuItem onClickHandler={action('clicked')} disabled={boolean('Disabled', false)} label={text('Text 1', 'Context menu item 1')} />
     <ContextMenuItem onClickHandler={action('clicked')} disabled={boolean('Disabled', false)} label={text('Text 2', 'Context menu item 2')} />
     <ContextMenuItem onClickHandler={action('clicked')} disabled={boolean('Disabled', false)} label={text('Text 3', 'Context menu item 3')} />
