6f8e3b32c5f3c77f2d4ce7576bb8353480791572
Alan <a.woosnam@ultimaker.com>
2018-08-29 14:39:48 +0200

    add Loading snapshot

---
+++ src/components/__tests__/loading.test.tsx
@@ -0,0 +1,25 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import 'jest';
+import 'jsdom-global/register';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+// component
+import Loading from '../loading';
+
+describe('The Loading component', () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      label: 'loading',
+    };
+    wrapper = shallow(<Loading {...props} />);
+  });
+
+  it('should render', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
