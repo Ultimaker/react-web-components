cd1e352cde3f6009fc05cbbcefcd14ab35571f95
Alan <a.woosnam@ultimaker.com>
2018-08-29 17:14:14 +0200

    add Spinner snapshot

---
+++ src/components/__tests__/spinner.test.tsx
@@ -0,0 +1,21 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import 'jest';
+import 'jsdom-global/register';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+// component
+import Spinner from '../spinner';
+
+describe('The Spinner component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Spinner />);
+  });
+
+  it('should render', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
