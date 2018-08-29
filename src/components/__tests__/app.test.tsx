899cee517462d3185d1ebf44eaeb73e59260601f
Alan <a.woosnam@ultimaker.com>
2018-08-29 11:02:25 +0200

    add App snapshot

---
+++ src/components/__tests__/app.test.tsx
@@ -0,0 +1,25 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import 'jest';
+import 'jsdom-global/register';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+// component
+import App from '../app';
+
+describe('The App component', () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      fixedHeader: true
+    };
+    wrapper = shallow(<App {...props} />);
+  });
+
+  it('should render', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
