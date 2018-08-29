be4dedcca7dac72143c67f21ac5a3436ca2cb815
Alan <a.woosnam@ultimaker.com>
2018-08-29 16:21:18 +0200

    add PanelArrow snapshot

---
+++ src/components/__tests__/panel_arrow.test.tsx
@@ -0,0 +1,25 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import 'jest';
+import 'jsdom-global/register';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+// component
+import PanelArrow from '../panel_arrow';
+
+describe('The PanelArrow component', () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      width: '20px'
+    };
+    wrapper = shallow(<PanelArrow {...props} />);
+  });
+
+  it('should render', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
