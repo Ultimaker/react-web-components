be7f3dcd4bb6d2c1f6a1fba4dda7955ff79069ae
Alan <a.woosnam@ultimaker.com>
2018-08-29 17:10:17 +0200

    add SlideInPanel snapshot

---
+++ src/components/__tests__/slide_in_panel.test.tsx
@@ -0,0 +1,29 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import 'jest';
+import 'jsdom-global/register';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+// component
+import SlideInPanel from '../slide_in_panel';
+
+describe('The SlideInPanel component', () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      headerTitle: 'Panel title',
+      isOpen: false,
+    };
+    wrapper = shallow(<SlideInPanel {...props}>
+      <div>Body</div>
+      <div>Footer</div>
+    </SlideInPanel>);
+  });
+
+  it('should render', () => {
+    expect(wrapper.render()).toMatchSnapshot();
+  });
+
+});
