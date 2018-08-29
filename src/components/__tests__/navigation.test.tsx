fef87f256417744b81d58b5feb37e0470a4fed38
Alan <a.woosnam@ultimaker.com>
2018-08-29 16:10:38 +0200

    add Navigation snapshot

---
+++ src/components/__tests__/navigation.test.tsx
@@ -0,0 +1,36 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import 'jest';
+import 'jsdom-global/register';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { BrowserRouter } from 'react-router-dom';
+
+// component
+import Navigation from '../navigation';
+
+// views
+import PageNotFoundView from '../../views/page_not_found';
+
+describe('The Navigation component', () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      navLinks: [
+        {
+          path: '/home',
+          label: 'Home',
+          visible: true,
+          component: PageNotFoundView
+        }
+      ]
+    };
+    wrapper = shallow(<BrowserRouter><Navigation {...props} /></BrowserRouter>);
+  });
+
+  it('should render', () => {
+    expect(wrapper.render()).toMatchSnapshot();
+  });
+
+});
