f2f5f2e81418f512c73c6b42f33f5169d94bf9e4
Alan <a.woosnam@ultimaker.com>
2018-08-29 16:34:42 +0200

    add ProgressBar snapshot

---
+++ src/components/__tests__/progress.test.tsx
@@ -0,0 +1,25 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import 'jest';
+import 'jsdom-global/register';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+// component
+import ProgressBar from '../progress_bar';
+
+describe('The ProgressBar component', () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      progressPercentage: 50
+    };
+    wrapper = shallow(<ProgressBar {...props} />);
+  });
+
+  it('should render', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
