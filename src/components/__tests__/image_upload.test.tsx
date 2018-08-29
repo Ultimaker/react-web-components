152b01a38838aae85ab0bdaec42ab8c19b8a9755
Alan <a.woosnam@ultimaker.com>
2018-08-29 13:18:31 +0200

    add ImageFile snapshot

---
+++ src/components/__tests__/image_upload.test.tsx
@@ -0,0 +1,26 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import 'jest';
+import 'jsdom-global/register';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+// component
+import ImageFile from '../image_upload';
+
+describe('The ImageFile component', () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      onFileSelection: jest.fn(),
+    };
+    wrapper = shallow(<ImageFile {...props} />);
+  });
+
+  it('should render', () => {
+    expect(wrapper).toMatchSnapshot();
+    expect(props.onFileSelection).not.toHaveBeenCalled();
+  });
+
+});
