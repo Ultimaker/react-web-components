72b56852dbf781f9364ea1b832268c1969cd1749
Alan <a.woosnam@ultimaker.com>
2018-08-29 11:49:56 +0200

    add FileUpload snapshot

---
+++ src/components/__tests__/file_upload.test.tsx
@@ -0,0 +1,27 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import 'jest';
+import 'jsdom-global/register';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+// component
+import FileUpload from '../file_upload';
+
+describe('The FileUpload component', () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      id: 'testFileUpload',
+      onChangeHandler: jest.fn(),
+    };
+    wrapper = shallow(<FileUpload {...props} />);
+  });
+
+  it('should render', () => {
+    expect(wrapper).toMatchSnapshot();
+    expect(props.onChangeHandler).not.toHaveBeenCalled();
+  });
+
+});
