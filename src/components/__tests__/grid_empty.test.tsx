1c56dfd6aa0b3076aa295911a1eab9662d1b54f1
Alan <a.woosnam@ultimaker.com>
2018-08-29 11:58:43 +0200

    add GridEmpty snapshot

---
+++ src/components/__tests__/grid_empty.test.tsx
@@ -0,0 +1,25 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import 'jest';
+import 'jsdom-global/register';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+// component
+import GridEmpty from '../grid_empty';
+
+describe('The GridEmpty component', () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      label: 'Empty grid'
+    };
+    wrapper = shallow(<GridEmpty {...props} />);
+  });
+
+  it('should render', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
