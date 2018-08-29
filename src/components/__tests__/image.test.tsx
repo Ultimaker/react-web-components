82815044cce5deebaf012d992b380b48c1c229ab
Alan <a.woosnam@ultimaker.com>
2018-08-29 13:29:18 +0200

    add Image snapshot

---
+++ src/components/__tests__/image.test.tsx
@@ -0,0 +1,25 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import 'jest';
+import 'jsdom-global/register';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+// component
+import Image from '../image';
+
+describe('The Image component', () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      src: './test_image.png'
+    };
+    wrapper = shallow(<Image {...props} />);
+  });
+
+  it('should render', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
