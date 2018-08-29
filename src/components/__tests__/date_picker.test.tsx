3151749a7827ea11cf6ae5302a44ed5ffdb24010
Alan <a.woosnam@ultimaker.com>
2018-08-29 11:36:25 +0200

    add date picker snapshot

---
+++ src/components/__tests__/date_picker.test.tsx
@@ -0,0 +1,28 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import 'jest';
+import 'jsdom-global/register';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+// component
+import DatePicker from '../date_picker';
+
+describe('The DatePicker component', () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      id: 'testDatePicker',
+      value: false,
+      onChangeHandler: jest.fn(),
+    };
+    wrapper = shallow(<DatePicker {...props} />);
+  });
+
+  it('should render', () => {
+    expect(wrapper).toMatchSnapshot();
+    expect(props.onChangeHandler).not.toHaveBeenCalled();
+  });
+
+});
