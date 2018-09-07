02b89fc626552f4d8347931a1b824d1f0bfe4724
Alan <a.woosnam@ultimaker.com>
2018-09-07 10:37:00 +0200

    add more coverage to Popup

--- src/components/__tests__/popup.test.tsx
+++ src/components/__tests__/popup.test.tsx
@@ -6,6 +6,10 @@ import { shallow } from 'enzyme';
 
 // component
 import Popup from '../popup';
+import Button from '../button';
+
+// mocks
+import { mockClickEvent } from '../../__mocks__/clickMock'
 
 describe('The Popup component', () => {
     let props;
@@ -17,6 +21,7 @@ describe('The Popup component', () => {
             bodyText: 'Popup body',
             primaryBtnText: 'Primary button',
             primaryBtnHandler: jest.fn(),
+            secondaryBtnHandler: jest.fn()
         };
         wrapper = shallow(<Popup {...props} />);
     });
@@ -32,4 +37,38 @@ describe('The Popup component', () => {
         expect(wrapper).toMatchSnapshot();
         expect(props.primaryBtnHandler).not.toHaveBeenCalled();
     });
+
+    it('should call primaryBtnHandler when the popup is submitted', () => {
+        wrapper.find('.popup').simulate('submit', mockClickEvent);
+        expect(props.primaryBtnHandler).toHaveBeenCalled();
+        expect(mockClickEvent.preventDefault).toBeCalled();
+        expect(wrapper.state().primaryBtnSpinner).toEqual(true);
+    });
+
+    it('should call primaryBtnHandler when validation passes', () => {
+        wrapper.setProps({ validationHandler: (value) => {return null} });
+        wrapper.find('.popup').simulate('submit', mockClickEvent);
+        expect(props.primaryBtnHandler).toHaveBeenCalled();
+    });
+
+    it('should not call primaryBtnHandler when validation passes', () => {
+        wrapper.setProps({ validationHandler: (value) => {return 'Test validation error'} });
+        wrapper.find('.popup').simulate('submit', mockClickEvent);
+        expect(props.primaryBtnHandler).not.toHaveBeenCalled();
+    });
+
+    it('should render a secondary button', () => {
+        wrapper.setProps({
+            secondaryBtnText: 'secondaryBtnText'
+        });
+        expect(wrapper.find(Button)).toHaveLength(2);
+        expect(props.secondaryBtnHandler).not.toHaveBeenCalled();
+    });
+
+    it('should call secondaryBtnHandler', () => {
+        wrapper.instance()._secondaryBtnHandler();
+        expect(props.secondaryBtnHandler).toHaveBeenCalled();
+        expect(wrapper.state().secondaryBtnSpinner).toEqual(true);
+    });
+
 });
