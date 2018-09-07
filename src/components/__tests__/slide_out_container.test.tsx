cfe5d3a25d0716ccfe2851c2763c3b9f807226de
Alan <a.woosnam@ultimaker.com>
2018-09-07 09:43:37 +0200

    add full coverage for SlideOutContainer

--- src/components/__tests__/slide_out_container.test.tsx
+++ src/components/__tests__/slide_out_container.test.tsx
@@ -3,9 +3,14 @@ import 'jest';
 import 'jsdom-global/register';
 import * as React from 'react';
 import { shallow } from 'enzyme';
+import { UnmountClosed } from 'react-collapse';
 
 // component
 import SlideOutContainer from '../slide_out_container';
+import PanelArrow from '../panel_arrow';
+
+// mocks
+import { mockClickEvent } from '../../__mocks__/clickMock'
 
 describe('The SlideOutContainer component', () => {
     let props;
@@ -14,14 +19,35 @@ describe('The SlideOutContainer component', () => {
     beforeEach(() => {
         props = {
             isOpen: false,
-            onChangeHandler: jest.fn(),
+            onHeaderClick: jest.fn(),
+            headerText: 'Test panel'
         };
         wrapper = shallow(<SlideOutContainer {...props} />);
     });
 
     it('should render', () => {
         expect(wrapper).toMatchSnapshot();
-        expect(props.onChangeHandler).not.toHaveBeenCalled();
+        expect(props.onHeaderClick).not.toHaveBeenCalled();
+    });
+
+    it('calls onHeaderClick when the header clicked', () => {
+        wrapper.find('.slide-out-container__header').simulate('click', mockClickEvent);
+        expect(props.onHeaderClick).toHaveBeenCalled();
+    });
+
+    it('should render a PanelArrow', () => {
+        wrapper.setProps({ showHeaderPanelArrow: true });
+        expect(wrapper.find(PanelArrow)).toHaveLength(1);
+    });
+
+    it('should open the panel', () => {
+        wrapper.setProps({ isOpen: true });
+        expect(wrapper.find(UnmountClosed).prop('isOpened')).toEqual(true);
+    });
+
+    it('should hide the header panel', () => {
+        wrapper.setProps({ headerText: null });
+        expect(wrapper.find('.slide-out-container__header').exists()).toEqual(false);
     });
 
 });
