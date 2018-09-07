59cc9de89d0c3a080deaa048323b75925be06e7b
Alan <a.woosnam@ultimaker.com>
2018-09-07 11:13:02 +0200

    add more tests for TagsSelector

--- src/components/__tests__/tags_selector.test.tsx
+++ src/components/__tests__/tags_selector.test.tsx
@@ -13,7 +13,7 @@ describe('The TagsSelector component', () => {
 
     beforeEach(() => {
         props = {
-            value: ['tag 1', 'tag 2', 'tag 3'],
+            value: ['tag 1', 'tag 2'],
             onChangeHandler: jest.fn(),
         };
         wrapper = shallow(<TagsSelector {...props} />);
@@ -21,6 +21,71 @@ describe('The TagsSelector component', () => {
 
     it('should render', () => {
         expect(wrapper).toMatchSnapshot();
+        expect(props.onChangeHandler).not.toHaveBeenCalled();
+        expect(wrapper.state().tags).toEqual([
+            {
+                id: 'tag 1',
+                text: 'tag 1'
+            },
+            {
+                id: 'tag 2',
+                text: 'tag 2'
+            }
+        ]);
+    });
+
+    it('should add suggestions', () => {
+        wrapper.setProps({ suggestions: ['suggestion tag 1', 'suggestion tag 2'] });
+        expect(wrapper.state().suggestions).toEqual([
+            {
+                id: 'suggestion tag 1',
+                text: 'suggestion tag 1'
+            },
+            {
+                id: 'suggestion tag 2',
+                text: 'suggestion tag 2'
+            }
+        ]);
+    });
+
+    it('should handle adding a tag', () => {
+        wrapper.instance()._handleAddition({
+            id: 'tag 3',
+            text: 'tag 3'
+        });
+        expect(props.onChangeHandler).toHaveBeenCalledWith(['tag 1', 'tag 2', 'tag 3']);
+    });
+
+    it('should handle changing tags order', () => {
+        wrapper.instance()._handleDrag({
+            id: 'tag 1',
+            text: 'tag 1'
+        }, 0, 1);
+        expect(props.onChangeHandler).toHaveBeenCalledWith(['tag 2', 'tag 1']);
+    });
+
+    it('should handle deleting a tag', () => {
+        wrapper.instance()._handleDelete(1);
+        expect(props.onChangeHandler).toHaveBeenCalledWith(['tag 1']);
+    });
+
+    it('should disable the tags', () => {
+        wrapper.setProps({ disabled: true });
+        expect(wrapper.find('.disabled')).toHaveLength(1);
+
+        wrapper.instance()._handleAddition({
+            id: 'tag 3',
+            text: 'tag 3'
+        });
+        expect(props.onChangeHandler).not.toHaveBeenCalled();
+
+        wrapper.instance()._handleDrag({
+            id: 'tag 1',
+            text: 'tag 1'
+        }, 0, 1);
+        expect(props.onChangeHandler).not.toHaveBeenCalled();
+
+        wrapper.instance()._handleDelete(1);
         expect(props.onChangeHandler).not.toHaveBeenCalled();
     });
 
