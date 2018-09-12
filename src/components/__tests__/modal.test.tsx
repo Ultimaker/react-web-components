0f9296442010b539a0edf85f0187ad92708e12b0
Alan <a.woosnam@ultimaker.com>
2018-09-12 16:31:55 +0200

    add more tests for Modal

--- src/components/__tests__/modal.test.tsx
+++ src/components/__tests__/modal.test.tsx
@@ -1,9 +1,13 @@
 // Copyright (c) 2018 Ultimaker B.V.
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 // component
 import Modal from '../modal';
+
+// mocks
+import { mockClickEvent } from '../../__mocks__/clickMock';
+
 
 describe('The Modal component', () => {
     let props;
@@ -24,6 +28,33 @@ describe('The Modal component', () => {
     it('should render', () => {
         expect(wrapper.render()).toMatchSnapshot();
         expect(props.onOverlayClickHandler).not.toHaveBeenCalled();
+    });
+
+    it('should call onOverlayClickHandler when the overlay is clicked', () => {
+        wrapper.dive().find('.modal').simulate('click', mockClickEvent);
+        expect(props.onOverlayClickHandler).toHaveBeenCalled();
+        expect(mockClickEvent.stopPropagation).toBeCalled();
+    });
+
+    it('should call onOverlayClickHandler when the overlay is clicked and onOverlayClickHandler is not passed', () => {
+        wrapper.setProps({ onOverlayClickHandler: null });
+        wrapper.dive().find('.modal').simulate('click', mockClickEvent);
+        expect(props.onOverlayClickHandler).not.toHaveBeenCalled();
+        expect(mockClickEvent.stopPropagation).toBeCalled();
+    });
+
+    it('should set isOpen to true after mounting', () => {
+        expect(wrapper.state('isOpen')).toEqual(true);
+    });
+
+    it('should not allow the background to scroll when open', () => {
+        expect(document.body.classList.contains('noscroll')).toEqual(true);
+    });
+
+    it('should allow the background to scroll after unmount', () => {
+        const mountedWrapper = mount(<Modal {...props} />);
+        mountedWrapper.unmount();
+        expect(document.body.classList.contains('noscroll')).toEqual(false);
     });
 
 });
