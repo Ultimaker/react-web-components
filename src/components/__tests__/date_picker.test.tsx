c92e4a18901b2c987db7010bd10a590268e97386
Alan <a.woosnam@ultimaker.com>
2018-09-12 17:08:34 +0200

    add test to DatePicker

--- src/components/__tests__/date_picker.test.tsx
+++ src/components/__tests__/date_picker.test.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) 2018 Ultimaker B.V.
 import * as React from 'react';
 import { shallow } from 'enzyme';
+import moment = require('moment');
 
 // component
 import DatePicker from '../date_picker';
@@ -21,6 +22,11 @@ describe('The DatePicker component', () => {
     it('should render', () => {
         expect(wrapper).toMatchSnapshot();
         expect(props.onChangeHandler).not.toHaveBeenCalled();
+    });
+
+    it('should display an error state', () => {
+        wrapper.setProps({ error: true });
+        expect(wrapper.find('.error')).toHaveLength(1);
     });
 
 });
