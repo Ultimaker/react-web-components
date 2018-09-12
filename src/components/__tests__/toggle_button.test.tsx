9ba258945197d5cee6670c9c2c16e97fe3f7b715
Alan <a.woosnam@ultimaker.com>
2018-09-12 16:18:16 +0200

    add more tests to ToggleButton

--- src/components/__tests__/toggle_button.test.tsx
+++ src/components/__tests__/toggle_button.test.tsx
@@ -5,6 +5,10 @@ import { shallow } from 'enzyme';
 // component
 import ToggleButton from '../toggle_button';
 
+// mocks
+import { mockClickEvent } from '../../__mocks__/clickMock';
+
+
 describe('The Checkbox component', () => {
     let props;
     let wrapper;
@@ -21,6 +25,11 @@ describe('The Checkbox component', () => {
     it('should render', () => {
         expect(wrapper).toMatchSnapshot();
         expect(props.onChangeHandler).not.toHaveBeenCalled();
+    });
+
+    it('should not propagate click', () => {
+        wrapper.simulate('click', mockClickEvent);
+        expect(mockClickEvent.stopPropagation).toBeCalled();
     });
 
 });
