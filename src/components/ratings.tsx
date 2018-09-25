3d43b7a0d90658cd7fa84b421f225004689a151c
Daniel Schiavini <d.schiavini@ultimaker.com>
2018-09-25 16:03:13 +0200

    Review comments (knobs & defaultProps)

--- src/components/ratings.tsx
+++ src/components/ratings.tsx
@@ -1,12 +1,13 @@
 // Copyright (c) 2018 Ultimaker B.V.
 import * as React from 'react';
-import {IconColor, IconWrapperProps} from './icons/icon_wrapper'
+import {IconColor, IconSize, IconWrapperProps} from './icons/icon_wrapper'
 import _ = require('lodash');
 
 export interface RatingsProps {
     max: number;
     rating: number | null;
     Icon: React.StatelessComponent<IconWrapperProps>;
+    size?: IconSize;
     foregroundColor?: IconColor;
     backgroundColor?: IconColor;
 }
@@ -18,25 +19,31 @@ export interface RatingsProps {
  * @param Icon - The icon class to use. It should be wrapped by `IconWrapper`.
  * @param foregroundColor - The color of the foreground icons. Defaults to 'orange'.
  * @param backgroundColor - The color of the background icons. Defaults to 'grey'.
+ * @param size - The size of the icons. Defaults to 'sm' (small).
  * @constructor
  */
 export const Ratings: React.StatelessComponent<RatingsProps> = (
-    { max, rating, Icon, foregroundColor = "orange", backgroundColor = "grey" }
+    { max, rating, Icon, foregroundColor, backgroundColor, size }
 ) => {
     return (
         <div className="ratings">
             <div className="ratings--background">
-                {_.times(max, i => <Icon key={i} color={backgroundColor} />)}
+                {_.times(max, i => <Icon key={i} color={backgroundColor} size={size} />)}
             </div>
-            {rating &&
+            {rating > 0 &&
                 <div className="ratings--foreground" style={{width: (100 * rating / max) + "%"}}>
-                    {_.times(max, i => <Icon key={i} color={foregroundColor}/>)}
+                    {_.times(max, i => <Icon key={i} color={foregroundColor} size={size} />)}
                 </div>
             }
         </div>
     );
 };
 
+Ratings.defaultProps = {
+    foregroundColor: "orange",
+    backgroundColor: "grey",
+    size: 'sm',
+};
 Ratings.displayName = "Ratings";
 
 export default Ratings;
---
+++ src/stories/ratings.stories.tsx
@@ -0,0 +1,110 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import * as React from 'react';
+import {storiesOf} from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs, number, selectV2 } from '@storybook/addon-knobs/react';
+import styles from "@sambego/storybook-styles";
+
+import Ratings from '../components/ratings'
+
+import ApprovedIcon from '../components/icons/approved_icon';
+import EmailIcon from '../components/icons/email_icon';
+import HighlightIcon from '../components/icons/highlight_icon';
+import PasswordIcon from '../components/icons/password_icon';
+import PendingIcon from '../components/icons/pending_icon';
+import RejectedIcon from '../components/icons/rejected_icon';
+import WarningIcon from '../components/icons/warning_icon';
+import WebsiteIcon from '../components/icons/website_icon';
+import DownloadIcon from '../components/icons/download_icon';
+import UploadIcon from '../components/icons/upload_icon';
+import LinkIcon from '../components/icons/link_icon';
+import DraftIcon from '../components/icons/draft_icon';
+import PublishedIcon from '../components/icons/published_icon';
+import UnpublishedIcon from '../components/icons/unpublished_icon';
+import RequiredIcon from '../components/icons/required_icon';
+import ProfileIcon from '../components/icons/profile_icon';
+import BuildPlateIcon from '../components/icons/build_plate_icon';
+import CameraIcon from '../components/icons/camera_icon';
+import DeleteIcon from '../components/icons/delete_icon';
+import DisabledIcon from '../components/icons/disabled_icon';
+import TransferIcon from '../components/icons/transfer_icon';
+import MaintenanceIcon from '../components/icons/maintenance_icon';
+import SingleArrowIcon from '../components/icons/single_arrow_icon';
+import DoubleArrowIcon from '../components/icons/double_arrow_icon';
+import PausedIcon from '../components/icons/paused_icon';
+import SegmentIcon from '../components/icons/segment_icon';
+import SettingsIcon from '../components/icons/settings_icon';
+import Um3PrinterIcon from '../components/icons/um3_printer_icon';
+import Um3xPrinterIcon from '../components/icons/um3x_printer_icon';
+import Ums5PrinterIcon from '../components/icons/ums5_printer_icon';
+
+const stories = storiesOf('Ratings', module);
+
+const iconOptions = {
+    approved: ApprovedIcon,
+    email: EmailIcon,
+    highlight: HighlightIcon,
+    password: PasswordIcon,
+    pending: PendingIcon,
+    rejected: RejectedIcon,
+    warning: WarningIcon,
+    website: WebsiteIcon,
+    download: DownloadIcon,
+    upload: UploadIcon,
+    link: LinkIcon,
+    draft: DraftIcon,
+    published: PublishedIcon,
+    unpublished: UnpublishedIcon,
+    required: RequiredIcon,
+    profile: ProfileIcon,
+    build_plate: BuildPlateIcon,
+    camera: CameraIcon,
+    delete: DeleteIcon,
+    disabled: DisabledIcon,
+    transfer: TransferIcon,
+    maintenance: MaintenanceIcon,
+    single_arrow: SingleArrowIcon,
+    double_arrow: DoubleArrowIcon,
+    paused: PausedIcon,
+    segment: SegmentIcon,
+    settings: SettingsIcon,
+    um3_printer: Um3PrinterIcon,
+    um3x_printer: Um3xPrinterIcon,
+    ums5_printer: Ums5PrinterIcon,
+}
+const colorOptions = {
+    'black': 'black',
+    'blue': 'blue',
+    'red': 'red',
+    'green': 'green',
+    'orange': 'orange',
+    'grey': 'grey',
+    'white': 'white'
+};
+
+const sizeOptions = {
+    'sm': 'sm',
+    'md': 'md',
+    'lg': 'lg'
+};
+
+stories.addDecorator(withKnobs)
+    .addDecorator(styles({
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+    }));
+
+stories.add('Ratings', withInfo(
+    'Ratings component'
+)(() =>
+    <Ratings
+        max={number("Maximum rating", 5)}
+        rating={number("Current rating", 3.5)}
+        Icon={iconOptions[selectV2("Icon", Object.keys(iconOptions), 'highlight')]}
+        foregroundColor={selectV2("Foreground color", colorOptions, Ratings.defaultProps.foregroundColor)}
+        backgroundColor={selectV2("Background color", colorOptions, Ratings.defaultProps.backgroundColor)}
+        size={selectV2("Size", sizeOptions, Ratings.defaultProps.size)}
+    />
+));
