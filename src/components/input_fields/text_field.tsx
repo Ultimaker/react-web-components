c3a779599072d191f2ad528d63d8950056eb2fc5
Daniel Schiavini <d.schiavini@ultimaker.com>
2018-09-30 11:25:36 +0200

    Experience with using wrapper as component

--- src/components/input_fields/text_field.tsx
+++ src/components/input_fields/text_field.tsx
@@ -1,43 +1,41 @@
-import * as React from 'react'
-import classNames from 'classnames'
+import * as React from 'react';
+import classNames from 'classnames';
 
-import InputFieldWrapper, {InputFieldProps, StaticFieldProps} from './input_field_wrapper'
+import InputFieldWrapper, { InputFieldProps } from './input_field_wrapper';
 
-export interface BaseTextFieldProps {
+export interface TextFieldProps extends  InputFieldProps {
     type: 'text' | 'password' | 'email' | 'url';
 }
 
-export interface TextFieldProps extends  InputFieldProps, BaseTextFieldProps {}
-
 const TextField: React.StatelessComponent<TextFieldProps> = ({
-    id, type, placeholder, value, showValidationError, onChangeHandler, inputRef
-}) =>
-    <input
-        id={id}
-        className={classNames('input', {'error': showValidationError})}
-        name={id}
-        type={type}
-        onChange={(e) => onChangeHandler(id, e.target.value)}
-        placeholder={placeholder}
-        value={value != null ? value.toString() : ''}
-        ref={inputRef}
-    />
+    id, type, placeholder, value, showValidationError, onChangeHandler, inputRef, ...props
+}) => {
+    const textField = () =>
+        <input
+            id={id}
+            className={classNames('input', {'error': showValidationError})}
+            name={id}
+            type={type}
+            onChange={(e) => onChangeHandler(id, e.target.value)}
+            placeholder={placeholder}
+            value={value != null ? value.toString() : ''}
+            ref={inputRef}
+        />;
+    const staticTextField = () => {
+        switch (type) {
+            case "email":
+                return <a href={`mailto:${value}`} target="_top">{value}</a>;
+            case "url":
+                return <a href={value.toString()} target="_blank">{value}</a>;
+            case "password":
+                return <React.Fragment>{"*".repeat(typeof(value) === "string" ? value.length : 0)}</React.Fragment>;
+            default:
+                return <React.Fragment>{value}</React.Fragment>;
+        }
+    }
+    return <InputFieldWrapper {...props} staticRender={staticTextField()}>{textField()}</InputFieldWrapper>
+}
 
 TextField.displayName = "TextField";
 
-export interface StaticTextFieldProps extends  StaticFieldProps, BaseTextFieldProps {}
-
-const StaticTextField: React.StatelessComponent<StaticTextFieldProps> = ({value, type}) => {
-    switch (type) {
-        case "email":
-            return <a href={`mailto:${value}`} target="_top">{value}</a>
-        case "url":
-            return <a href={value.toString()} target="_blank">{value}</a>
-        case "password":
-            return <React.Fragment>{"*".repeat(typeof(value) === "string" ? value.length : 0)}</React.Fragment>
-        default:
-            return <React.Fragment>{value}</React.Fragment>
-    }
-}
-
-export default InputFieldWrapper(TextField, StaticTextField)
+export default TextField;
