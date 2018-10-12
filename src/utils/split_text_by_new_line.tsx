91694e19d5054d353e555c846d0ebb168df27263
Daniel Schiavini <d.schiavini@ultimaker.com>
2018-10-12 09:30:07 +0200

    Using css to fix static textarea

--- src/utils/split_text_by_new_line.tsx
+++ src/utils/split_text_by_new_line.tsx
@@ -6,7 +6,7 @@ import * as React from 'react';
  */
 function splitTextByNewLine(text: string): JSX.Element[] {
     if (text) {
-        return text.split("\n").map((p, i) => <p key={i}>{p || "&nbsp;"}</p>);
+        return text.split("\n").map((p, i) => <p key={i}>{p}</p>);
     }
     return null;
 }
