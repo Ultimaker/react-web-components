54c842da4f272e3cb811a8ccdc09167f15d923a8
Daniel Schiavini <d.schiavini@ultimaker.com>
2018-11-14 11:25:58 +0100

    STAR-299: Static field

---
+++ src/components/input_fields/__tests__/static_field.test.tsx
@@ -0,0 +1,35 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+// components
+import StaticField, {StaticFieldProps} from '../static_field';
+import InputFieldWrapper from '../input_field_wrapper';
+
+describe('The static field component', () => {
+    let props: StaticFieldProps;
+    let wrapper;
+
+    beforeEach(() => {
+        props = {
+            id: 'testInputField',
+            value: "2018",
+            labelLayoutWidth: '1/1',
+            labelWidthBreakpoint: 'sm',
+        };
+        wrapper = shallow(<StaticField {...props}>child</StaticField>);
+    });
+
+    it('should render an input wrapper', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("should render it's value and its children", () => {
+        expect(wrapper.children().map(c => c.text())).toEqual(["2018", "child"]);
+    });
+
+    it('should render a null', () => {
+        wrapper.setProps({value: null})
+        expect(wrapper.find(InputFieldWrapper).prop("value")).toBeNull();
+    })
+});
---
+++ src/components/input_fields/static_field.tsx
@@ -0,0 +1,28 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import * as React from 'react';
+
+import {InputFieldProps} from './input_field_wrapper';
+import InputFieldWrapper from './input_field_wrapper'
+
+export interface StaticFieldProps extends InputFieldProps {
+    /** Input field value */
+    value: any;
+    /** Optional extra elements to be displayed after the input **/
+    children?: any;
+}
+
+/**
+ * The static field does not have change handling and simply outputs it's value and children.
+ * @constructor
+ */
+const StaticField: React.StatelessComponent<StaticFieldProps> = ({
+    children, staticField, ...wrapperProps
+}) =>
+    <InputFieldWrapper touched={false} staticField={true} inputChildren={children} {...wrapperProps}>
+        {wrapperProps.value}
+        {children}
+    </InputFieldWrapper>;
+
+StaticField.displayName = "StaticField";
+
+export default StaticField;
