3ee567035bf0435db4522c829ce08ff4f27bed35
Imre Bognar <i.bognar@ultimaker.com>
2018-11-26 17:59:56 +0100

    functionality set complete
    CL-1013

--- src/components/video_player.tsx
+++ src/components/video_player.tsx
@@ -1,59 +1,86 @@
 import * as React from 'react';
+/** this component knows much more than youtube, but only that is needed for now */
 import YouTubePlayer from 'react-player/lib/players/YouTube'
 
-import Loading from "./loading";
+import Spinner from "./spinner";
 
 export interface VideoPlayerProps {
+    /** url for the react-player component */
     url: string;
+    /** optional width to be given to the container and the video */
     width?: string;
+    /** optional height to be given to the container and the video */
     height?: string;
 }
 
 export interface VideoPlayerState {
     loading: boolean;
+    error: boolean;
+    wrongUrl: boolean;
 }
 
 export default class VideoPlayer extends React.Component<VideoPlayerProps, VideoPlayerState> {
+    static defaultProps = {
+        width: '100%',
+        height: '100%'
+    };
+
     state = {
-        loading: true
+        loading: true,
+        error: false,
+        wrongUrl: false
     }
 
-    private _ready(){
-        this.setState({loading:false})
+    private _ready() {
+        this.setState({ loading: false })
     }
 
-    private _hide(){
-        if (this.state.loading){
-            return {display : 'none'}
-        }else{
-            return {}
+    private _error() {
+        this.setState({ error: true })
+    }
+
+    private _playerClasses() {
+        if (this.state.loading) {
+            return 'video-player--player video-player--player__hidden'
+        } else {
+            return 'video-player--player'
         }
     }
 
-    componentDidUpdate(){
-
+    componentDidMount(){
+        if (!YouTubePlayer.canPlay(this.props.url)){
+            this.setState({
+                loading:false,
+                wrongUrl: true
+            });
+        }
     }
 
     render() {
         const { url, width, height } = this.props;
-        const { loading } = this.state;
+        const { loading, wrongUrl } = this.state;
 
         const playerProps = {
+            className: this._playerClasses(),
             onReady: () => this._ready(),
+            onError: () => this._error(),
             url: url,
             width: width ? width : '',
             height: height ? height : '',
         };
-        
+        const containerStyle = {
+            width: width ? width : '',
+            height: height ? height : '',
+        }
 
         return (
-            <div>
-                {!YouTubePlayer.canPlay(url) && 'Cannot play url'}
+            <div style={containerStyle} className='video_player'>
+                {wrongUrl && 'Can not load Url'}
 
-                {loading && <Loading label='Loading'/>}
+                {loading && <Spinner />}
 
-                <div style={this._hide()}><YouTubePlayer {...playerProps} /></div>
+                <YouTubePlayer {...playerProps} />
             </div>
-            );
+        );
     }
 }
