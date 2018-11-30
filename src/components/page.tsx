64f67f465823a63b39ba3364f08f5ef4652d95a7
Alan <a.woosnam@ultimaker.com>
2018-11-30 15:36:50 +0100

    add PageSection

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
-import PageTitle from './page_title';
 import classNames = require('classnames');
 
+// components
+import PageTitle from './page_title';
+
 export interface PageProps {
+    /** The page title displayed at the top of the page */
     title?: string;
+    /** The max width of the page content. The content will be centered horizontally on the page */
     maxWidth?: number | string;
+    /** Optional ID for the button **/
     id?: string;
+    /** Additional classes for styling */
     className?: string;
+    /** The page content */
+    children: any;
 }
 
 const Page: React.StatelessComponent<PageProps> =
