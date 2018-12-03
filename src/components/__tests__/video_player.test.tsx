06bc1560d02a2767d7e53dc06d873a065e24fb9b
Imre Bognar <i.bognar@ultimaker.com>
2018-12-03 12:20:58 +0100

    increase test coverage
    CL-1013

--- src/components/__tests__/video_player.test.tsx
+++ src/components/__tests__/video_player.test.tsx
@@ -1,6 +1,6 @@
 // Copyright (c) 2018 Ultimaker B.V.
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 // component
 import VideoPlayer, { VideoPlayerProps } from '../video_player';
@@ -15,17 +15,34 @@ describe('The video player component', () => {
         wrapper = shallow(<VideoPlayer {...props} />);
     });
 
-    it('should render', () => {
+    it('should render the loading state', () => {
         expect(wrapper).toMatchSnapshot();
     });
 
     it('should display the error state', () => {
-        wrapper.setState({ error: true })
+        wrapper.instance()._error();
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should display the loaded state', () => {
+        wrapper.instance()._ready();
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should display the loading state when the url is updated with a valid url', () => {
+        wrapper.instance()._ready();
+        wrapper.setProps({ url: 'https://www.youtube.com/watch?v=KI5pvThvu9I' })
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should stay in the loaded state when the url is updated with the original url', () => {
+        wrapper.instance()._ready();
+        wrapper.setProps({ url: 'https://www.youtube.com/watch?v=160yAufQ-is' })
         expect(wrapper).toMatchSnapshot();
     });
 
     it('should display a message in case the player can not play the url', () => {
-        wrapper.setState({ wrongUrl: true })
+        wrapper.setProps({ url: '|||invalid url |||' })
         expect(wrapper).toMatchSnapshot();
     });
 
