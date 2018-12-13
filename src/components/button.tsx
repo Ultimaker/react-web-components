b3db45de730f110744ad1f21bf30ce36b1e85367
Alan <a.woosnam@ultimaker.com>
2018-12-13 17:02:39 +0100

    ignore button issues
    STAR-339

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -83,18 +83,19 @@ export class Button extends React.Component<ButtonProps, {}> {
         } = this.props;
 
         return (
-            <a
-                id={id}
-                className={classes}
-                href={disabled || showSpinner ? undefined : linkURL}
-                target={linkToNewTab ? '_blank' : undefined}
-                rel="noopener noreferrer"
-            >
-                <span className="text">{children}</span>
-                {showSpinner
-                    && <Spinner />
-                }
-            </a>
+            <React.Fragment>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <a
+                    id={id}
+                    className={classes}
+                    href={disabled || showSpinner ? undefined : linkURL}
+                    target={linkToNewTab ? '_blank' : undefined}
+                    rel="noopener noreferrer"
+                >
+                    <span className="text">{children}</span>
+                    {showSpinner && <Spinner />}
+                </a>
+            </React.Fragment>
         );
     }
 
@@ -104,20 +105,22 @@ export class Button extends React.Component<ButtonProps, {}> {
         } = this.props;
 
         return (
-            <button
-                id={id}
-                className={classes}
-                onClick={this._onClickHandler}
-                disabled={disabled || showSpinner}
-                type={type}
-            >
-                <span className="text">{children}</span>
-                {showSpinner
-                    && <Spinner />
-                }
-            </button>
+            <React.Fragment>
+                {/* eslint-disable-next-line react/button-has-type */}
+                <button
+                    id={id}
+                    className={classes}
+                    onClick={this._onClickHandler}
+                    disabled={disabled || showSpinner}
+                    type={type}
+                >
+                    <span className="text">{children}</span>
+                    {showSpinner && <Spinner />}
+                </button>
+            </React.Fragment>
         );
     }
+
 
     render(): JSX.Element {
         const {
