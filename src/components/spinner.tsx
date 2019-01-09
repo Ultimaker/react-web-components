6f8452383d2023a962c7f9ae3ece3c8fc45b4de0
Alan <a.woosnam@ultimaker.com>
2019-01-09 16:52:23 +0100

    export spinner

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-const Spinner: React.StatelessComponent<{}> = () => (
+export const Spinner: React.StatelessComponent<{}> = () => (
     <div className="spinner">
         <svg width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
             <circle className="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30" />
