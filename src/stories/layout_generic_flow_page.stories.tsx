593fdde18b7549aa0ae1e73a65a9acef2f6bc0c4
Alan <a.woosnam@ultimaker.com>
2019-01-14 15:36:56 +0100

    add GenericFlowPage to storybook

---
+++ src/stories/layout_generic_flow_page.stories.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs/react';
+import { withInfo } from '@storybook/addon-info';
+
+import GenericFlowPage from '../components/generic_flow_page';
+import Spinner from '../components/spinner';
+
+const stories = storiesOf('Example layouts', module);
+
+stories.addDecorator(withKnobs);
+
+stories.add('Generic Flow Page', withInfo(
+    'A example layout for a flow page',
+)(() => (
+    <div style={{ marginTop: '6rem' }}>
+        <GenericFlowPage
+            title="Enabling cloud connectivity"
+            description="Waiting for the printer to enable cloud service, this can take a few seconds."
+            image={<img className="cloud-connecting-image" src="../images/cloud_connection/cloud_connecting-image.svg" alt="cloud-connecting" />}
+        >
+            <Spinner />
+        </GenericFlowPage>
+    </div>
+)));
