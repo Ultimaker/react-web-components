9715ab39f765aee1c80e75fa7b1f3efcb158021a
Alan <a.woosnam@ultimaker.com>
2019-01-16 14:56:53 +0100

    add align prop to FormActions

--- src/components/form_actions.tsx
+++ src/components/form_actions.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 
+export type FormActionsAlign = 'left' | 'center' | 'right';
+
 export interface FormActionsProps {
+    /** The horizontal alignment of the buttons */
+    align: FormActionsAlign;
     /** typically the form actions are a list of buttons */
     children: JSX.Element[] | JSX.Element;
 }
@@ -8,12 +12,20 @@ export interface FormActionsProps {
 /**
  * Form actions component that adds the right classes to a list of buttons after a form.
  */
-export const FormActions: React.StatelessComponent<FormActionsProps> = ({ children }) => (
-    <div className="form__actions">
-        {React.Children.toArray(children).map((child, index) => child
-            && <div key={index} className="form__btn-container">{child}</div>)}
-    </div>
-);
+export const FormActions: React.StatelessComponent<FormActionsProps> = ({ align, children }) => {
+    const classNames = (`form-actions form-actions--${align}`);
+
+    return (
+        <div className={classNames}>
+            {React.Children.toArray(children).map((child, index) => child
+                && <div key={index} className="form-actions__btn-container">{child}</div>)}
+        </div>
+    );
+};
+
+FormActions.defaultProps = {
+    align: 'left',
+};
 
 FormActions.displayName = 'FormActions';
 export default FormActions;
