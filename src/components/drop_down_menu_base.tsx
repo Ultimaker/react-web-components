204c7d383fd99e46af9c4276ca074e1769685cb7
Alan <a.woosnam@ultimaker.com>
2019-01-17 11:06:48 +0100

    add more tests to DropDownMenuBase
    STAR-374

--- src/components/drop_down_menu_base.tsx
+++ src/components/drop_down_menu_base.tsx
@@ -77,10 +77,6 @@ export class DropDownMenuBase extends React.Component<DropDownMenuBaseProps, {}>
             { 'drop-down-menu-base--visible': showMenu },
         );
 
-        const onCloseMenuHandlerProp = {
-            onCloseMenuHandler: () => this._onToggleMenuHandler(false),
-        };
-
         return (
             // eslint-disable-next-line max-len
             // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
