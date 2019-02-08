da5ddd8e85674663bbb702c0030e1c481e25e7c0
Alan <a.woosnam@ultimaker.com>
2019-02-08 15:38:57 +0100

    update tests
    STAR-427

--- src/components/input_fields/__tests__/date_field.test.tsx
+++ src/components/input_fields/__tests__/date_field.test.tsx
@@ -1,65 +1,67 @@
-// // Copyright (c) 2018 Ultimaker B.V.
-// import * as React from 'react';
-// import { shallow } from 'enzyme';
+// Copyright (c) 2018 Ultimaker B.V.
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import DatePicker from 'react-date-picker';
 
-// // component
-// import DateField, { DateFieldProps } from '../date_field';
-// import InputFieldWrapper from '../input_field_wrapper';
-// import DatePicker from '../../date_picker';
+// component
+import DateField, { DateFieldProps } from '../date_field';
+import InputFieldWrapper from '../input_field_wrapper';
 
-// describe('The date field component', () => {
-//     let props: DateFieldProps;
-//     let wrapper;
+describe('The date field component', () => {
+    let props: DateFieldProps;
+    let wrapper;
+    const testDate = new Date(0);
 
-//     beforeEach(() => {
-//         props = {
-//             id: 'testInputField',
-//             value: '2018-12-31',
-//             onChangeHandler: jest.fn(),
-//             labelLayoutWidth: '1/1',
-//             labelWidthBreakpoint: 'sm',
-//             staticField: false,
-//             placeholder: 'placeholder text',
-//         };
-//         wrapper = shallow(<DateField {...props} />);
-//     });
+    beforeEach(() => {
+        props = {
+            id: 'testInputField',
+            value: testDate,
+            onChangeHandler: jest.fn(),
+            labelLayoutWidth: '1/1',
+            labelWidthBreakpoint: 'sm',
+            staticField: false,
+            locale: 'en-GB',
+        };
+        wrapper = shallow(<DateField {...props} />);
+    });
 
-//     it('should render a wrapper', () => {
-//         expect(wrapper).toMatchSnapshot();
-//         expect(props.onChangeHandler).not.toHaveBeenCalled();
-//     });
+    it('should render a wrapper', () => {
+        expect(wrapper).toMatchSnapshot();
+        expect(props.onChangeHandler).not.toHaveBeenCalled();
+    });
 
-//     it('should render a date picker', () => {
-//         expect(wrapper.find(DatePicker).props()).toEqual({
-//             error: undefined,
-//             format: 'DD-MM-YYYY',
-//             id: 'testInputField',
-//             onChangeHandler: wrapper.instance()._onChange,
-//             placeholder: 'placeholder text',
-//             value: '2018-12-31',
-//         });
-//     });
+    it('should render a date picker', () => {
+        expect(wrapper.find(DatePicker).props()).toEqual({
+            calendarIcon: null,
+            clearIcon: null,
+            onChange: wrapper.instance()._onChange,
+            value: testDate,
+            isOpen: null,
+            locale: 'en-GB',
+            returnValue: 'start',
+        });
+    });
 
-//     it('should render an empty date', () => {
-//         wrapper.setProps({ value: null });
-//         expect(wrapper.find(DatePicker).prop('value')).toBeNull();
-//     });
+    it('should render an empty date', () => {
+        wrapper.setProps({ value: null });
+        expect(wrapper.find(DatePicker).prop('value')).toBeNull();
+    });
 
-//     it('should render a static date', () => {
-//         wrapper.setProps({ staticField: true });
-//         expect(wrapper.find(DatePicker)).toHaveLength(0);
-//         expect(wrapper.find(InputFieldWrapper).prop('children')).toEqual('31-12-2018');
-//     });
+    it('should render a static date', () => {
+        wrapper.setProps({ staticField: true });
+        expect(wrapper.find(DatePicker)).toHaveLength(0);
+        expect(wrapper.find(InputFieldWrapper).prop('children')).toEqual('1/1/1970');
+    });
 
-//     it('should render an error', () => {
-//         wrapper.setProps({ validationError: 'validation error', submitted: true });
-//         expect(wrapper.find(DatePicker).prop('error')).toEqual(true);
-//     });
+    it('should render an error', () => {
+        wrapper.setProps({ validationError: 'validation error', submitted: true });
+        expect(wrapper.find('.error')).toHaveLength(1);
+    });
 
-//     it('should call the callback', () => {
-//         expect(props.onChangeHandler).not.toHaveBeenCalled();
-//         wrapper.find(DatePicker).prop('onChangeHandler')('2016-02-29');
-//         expect(props.onChangeHandler).toHaveBeenCalledWith(props.id, '2016-02-29');
-//         expect(wrapper.find(InputFieldWrapper).prop('touched')).toEqual(true);
-//     });
-// });
+    it('should call the callback', () => {
+        expect(props.onChangeHandler).not.toHaveBeenCalled();
+        wrapper.find(DatePicker).prop('onChange')(testDate);
+        expect(props.onChangeHandler).toHaveBeenCalledWith(props.id, testDate);
+        expect(wrapper.find(InputFieldWrapper).prop('touched')).toEqual(true);
+    });
+});
