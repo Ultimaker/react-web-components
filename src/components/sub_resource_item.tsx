814c36541c44f39466ad34a3a5668ea8488d57df
Alan <a.woosnam@ultimaker.com>
2019-04-04 14:20:57 +0200

    add sub-resource components and styling

---
+++ src/components/sub_resource_item.tsx
@@ -0,0 +1,37 @@
+// Copyright (c) 2018 Ultimaker B.V.
+import * as React from 'react';
+
+// generated
+import Pill from './pill';
+import ProfileImage from './profile_image';
+import { Tooltip } from './tooltip';
+
+export interface SubResource {
+    subResourceId: string;
+    name: string;
+    iconUrl?: string;
+    showIcon?: boolean;
+    showName?: boolean;
+}
+
+export interface SubResourceItemProps {
+    subResource: SubResource
+}
+
+/**
+ * The sub-resource component shows a list of names/images.
+ */
+export const SubResourceItem: React.StatelessComponent<SubResourceItemProps> = ({
+    subResource,
+}) => (
+    <Tooltip tooltipText={subResource.name}>
+        <div className="sub-resource-item">
+            {subResource.showIcon && <ProfileImage size="3rem" imageURL={subResource.iconUrl} />}
+            {subResource.showName && <Pill>{subResource.name}</Pill>}
+        </div>
+    </Tooltip>
+);
+
+SubResourceItem.displayName = 'SubResourceItem';
+
+export default SubResourceItem;
