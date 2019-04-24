0a568595fe19eb2079951909d4b08fba6eabaeae
Alan <a.woosnam@ultimaker.com>
2019-04-24 13:39:25 +0200

    Add some more header tests

--- src/components/__tests__/header.test.tsx
+++ src/components/__tests__/header.test.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 
 // component
+import Navigation from '../navigation';
 import Header from '../header';
 import Button from '../button';
 
@@ -17,11 +18,59 @@ describe('The Header component', () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-    it('should toggle the mobile navigation', () => {
-        expect(wrapper.state('showMobileMenu')).toBe(false);
+    it('should add props to navigation component', () => {
+        const navProps = {
+            navLinks: [
+                {
+                    path: '/home',
+                    label: 'Home',
+                    visible: true,
+                },
+            ],
+            accountNavText: 'Account',
+            signOutNavText: 'Sign out',
+            onCloseMobileMenuHandler: null,
+            showMobileMenu: null,
+        };
+        wrapper = shallow(
+            <Header>
+                <Navigation {...navProps} />
+            </Header>,
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should be able to use props to navigation component', () => {
+        const navProps = {
+            navLinks: [
+                {
+                    path: '/home',
+                    label: 'Home',
+                    visible: true,
+                },
+            ],
+            accountNavText: 'Account',
+            signOutNavText: 'Sign out',
+            onCloseMobileMenuHandler: null,
+            showMobileMenu: null,
+        };
+        wrapper = shallow(
+            <Header>
+                <Navigation {...navProps} />
+            </Header>,
+        );
+
+        expect(wrapper.find(Navigation).props().showMobileMenu).toBe(false);
         wrapper.find(Button).props().onClickHandler();
-        expect(wrapper.state('showMobileMenu')).toBe(true);
-        wrapper.find(Button).props().onClickHandler();
-        expect(wrapper.state('showMobileMenu')).toBe(false);
+        expect(wrapper.find(Navigation).props().showMobileMenu).toBe(true);
+        wrapper.find(Navigation).props().onCloseMobileMenuHandler();
+        expect(wrapper.find(Navigation).props().showMobileMenu).toBe(false);
+    });
+
+    it('should render the user account menu', () => {
+        wrapper.setProps({
+            userAccountMenu: <div className="test-user-account-menu" />,
+        });
+        expect(wrapper).toMatchSnapshot();
     });
 });
