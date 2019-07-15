a9a9f0482c8ffbcb6c37af69fe2de39e9854943b
Imre Bognar <i.bognar@ultimaker.com>
2019-07-15 16:56:00 +0200

    clean up icon

--- src/components/icons/tick_icon.tsx
+++ src/components/icons/tick_icon.tsx
@@ -14,10 +14,10 @@ const TickIcon: React.StatelessComponent<IconProps> = ({ className }): JSX.Eleme
             viewBox="0 0 32 32"
             enableBackground="new 0 0 32 32"
         >
-            <g id="Cura-Marketplace--current">
-                <g id="A4-Copy-3" transform="translate(-509.000000, -368.000000)">
-                    <g id="Group" transform="translate(510.000000, 369.000000)">
-                        <g id="Path">
+            <g>
+                <g transform="translate(-509.000000, -368.000000)">
+                    <g transform="translate(510.000000, 369.000000)">
+                        <g>
                             <polygon points="10.3,27.1 -0.4,16.4 2.4,13.6 10.3,21.5 27.6,4.3 30.4,7.1" />
                         </g>
                     </g>
