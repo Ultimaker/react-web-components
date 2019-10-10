d3234e6ef493714b9a90e235d4288334ce94fefc
Matijs Brinkhuis <matijs@gmail.com>
2019-10-10 10:43:02 +0200

    Add @ultimaker/stylelint-config and rc file

---
+++ .stylelintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: '@ultimaker/stylelint-config/scss',
+    rules: {
+        'at-rule-blacklist': null,
+        'declaration-property-unit-blacklist': null,
+        'max-nesting-depth': null,
+        'no-descending-specificity': null,
+        'property-no-vendor-prefix': null,
+        'value-no-vendor-prefix': null,
+    },
+};
+
