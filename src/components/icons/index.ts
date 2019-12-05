54e115473811c1439fb267d827c2ee24bcdc4ea0
Ian Paschal <ianpaschal@protonmail.com>
2019-12-05 09:40:20 +0100

    Different format for SVG icons
    CS-523

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,2 +1,4 @@
 export { MagnifierIconSmall as MagnifierSmall } from './magnifier_icon_small';
 export { MagnifierIcon as Magnifier } from './magnifier_icon';
+export { EyeIconSmall as EyeIconSmall } from './eye_icon_small';
+export { EyeIcon as EyeIcon } from './eye_icon';
